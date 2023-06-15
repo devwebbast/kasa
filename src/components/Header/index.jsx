9eb1f2361d936492f509b9a7d2f4d607e83f085b
DevBastFront <devbastfront@gmail.com>
2023-06-15 04:10:32 +0200

    V1.0.2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 import logo from '../../images/logo.svg'
-import './header.css'
+import '../../sass/main.scss'
 function Header (){
     return(
         <nav>
             <div className="header-style">
                 <img className='logo' src={logo} alt='Logo Kasa' />
                 <div className="nav-link">
-                    <Link to="/">Accueil</Link>
+                    <Link className="nav-link-home" to="/">Accueil</Link>
                     <Link to="/about">A propos</Link>
                 </div>
             </div>
--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
-import './error.css'
+import '../../sass/main.scss'
 
 function Error (){
     return(
         <div className="error-style">
-            <div>
-                <h2>404</h2>
+            <div className='error-title'>
+                <h2 className='nbrError'>404</h2>
             </div>
             <div>
                 <h3>Oups! La page que vous demandez n'existe pas.</h3>
